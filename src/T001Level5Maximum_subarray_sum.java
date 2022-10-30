e1eeaf63a51fd8d6a3ee3a729f689728f3b82128
zuzayka <bool6@yandex.ru>
2022-10-30 15:23:50 +0300

    probe commit codewars

--- src/T001Level5Maximum_subarray_sum.java
+++ src/T001Level5Maximum_subarray_sum.java
@@ -4,6 +4,6 @@ public class T001Level5Maximum_subarray_sum {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello worldd!");
     }
 }
