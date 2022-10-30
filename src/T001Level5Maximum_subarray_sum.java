780e11448096f4c079b5f6021015c0d676263f02
zuzayka <bool6@yandex.ru>
2022-10-30 13:43:53 +0300

    first commit codewars

---
+++ src/T001Level5Maximum_subarray_sum.java
@@ -0,0 +1,9 @@
+public class T001Level5Maximum_subarray_sum {
+    public static int sequence(int[] arr) {
+        return 4;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
