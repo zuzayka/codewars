31092aa89cb9b009cda39970874424eb078ed8b4
zuzayka <bool6@yandex.ru>
2022-11-16 11:33:09 +0300

    T004 commit

---
+++ src/T004_Level6_CamelCase_Method.java
@@ -0,0 +1,19 @@
+public class T004_Level6_CamelCase_Method {
+    public static String camelCase(String str) {
+        if (str.length() > 0) {
+            String[] arr = str.split(" ");
+            StringBuilder sbOut = new StringBuilder();
+            for (String s : arr) {
+                if (s.length() > 0) {
+                    sbOut.append(s.substring(0, 1).toUpperCase()).append(s.substring(1));
+                }
+            }
+            return sbOut.toString();
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(camelCase(" a asf"));
+    }
+}
